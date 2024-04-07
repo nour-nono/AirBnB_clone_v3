53717c512a1bff7b8b020f8a0b3e3e61403ae9c6
nono <nour98nour@gmail.com>
2024-04-07 03:18:07 +0200

    added some code may solve the problem

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """api status"""
+import models
 from models import storage
+from models.base_model import BaseModel
 from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
 def status_all_obj():
-	"""Return status for all obj"""
-	classes = ["Amenity", "City", "Place", "Review", "State", "User"]
-	dd = {}
-	for c in classes:
-		dd[c] = storage.count(c)
-	return jsonify(dd)
+    """Return status for all obj"""
+    classes = {'states': State, 'users': User,
+               'amenities': Amenity, 'cities': City,
+               'places': Place, 'reviews': Review}
+    dd = {}
+    for c in classes:
+        dd[c] = storage.count(c)
+    return jsonify(dd)
