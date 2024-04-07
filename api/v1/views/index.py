39ee4f3cff6e1bcbcaad275855421513da9fe9c7
nono <nour98nour@gmail.com>
2024-04-07 03:28:24 +0200

    found the wrong

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """api status"""
-import models
 from models import storage
-from models.base_model import BaseModel
 from flask import jsonify
 from api.v1.views import app_views
 
@@ -10,10 +8,10 @@ from api.v1.views import app_views
 @app_views.route('/status', strict_slashes=False)
 def status_all_obj():
     """Return status for all obj"""
-    classes = {'states': State, 'users': User,
-               'amenities': Amenity, 'cities': City,
-               'places': Place, 'reviews': Review}
-    dd = {}
-    for c in classes:
-        dd[c] = storage.count(c)
+    dd = {"amenities": storage.count("Amenity"),
+          "cities": storage.count("City"),
+          "places": storage.count("Place"),
+          "reviews": storage.count("Review"),
+          "states": storage.count("State"),
+          "users": storage.count("User")}
     return jsonify(dd)
