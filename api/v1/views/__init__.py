a388fee0c88bc78594645d468bf6d4c77df285d8
nono <nour98nour@gmail.com>
2024-04-07 02:13:25 +0200

    my code again

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""Module for views"""
 from flask import Blueprint
 
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,9 +8,9 @@ from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
-def returnstuff():
-    """return stuff"""
-    return jsonify(status='OK')
+def status_obj():
+	"""Return status"""
+	return jsonify(status="OK")
 
 
 # @app_views.route('/stats', strict_slashes=False)
