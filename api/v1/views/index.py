1a932412cd6811ae260f1e4aa5a07a43165ec2e5
nono <nour98nour@gmail.com>
2024-04-07 02:51:20 +0200

    status_all_obj

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """api status"""
+from models import storage
 from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
-def status_obj():
+def status_ok():
 	"""Return status"""
 	return jsonify(status="OK")
+
+
+@app_views.route('/status', strict_slashes=False)
+def status_all_obj():
+	"""Return status for all obj"""
+	classes = ["Amenity", "City", "Place", "Review", "State", "User"]
+	dd = {}
+	for c in classes:
+		dd[c] = storage.count(c)
+	return jsonify(dd)
