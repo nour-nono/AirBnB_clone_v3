2c9db1e7c68870108c0cdd0b4c350fb6eaba29a0
nono <nour98nour@gmail.com>
2024-04-07 02:02:28 +0200

    tests uncommented

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
-"""Main module"""
-from flask import Flask
-import os
-from api.v1.views import app_views
+"""Flask server (variable app)
+"""
+
+
+from flask import Flask, jsonify
 from models import storage
+from os import getenv
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def close():
-    """Close the session"""
+def downtear(self):
+    '''Status of your API'''
     storage.close()
 
 
-if __name__ == '__main__':
-    host = '0.0.0.0'
-    port = 5000
-    if os.getenv('HBNB_API_HOST'):
-        host = os.getenv('HBNB_API_HOST')
-    if os.getenv('HBNB_API_PORT'):
-        port = os.getenv('HBNB_API_PORT')
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error='Not found'), 404
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
