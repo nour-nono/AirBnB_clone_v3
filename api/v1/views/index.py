40503b8433a86ca43949a6f1cc02bc382cbc02ec
nono <nour98nour@gmail.com>
2024-04-07 02:59:03 +0200

    only one route status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,12 +6,6 @@ from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
-def status_ok():
-	"""Return status"""
-	return jsonify(status="OK")
-
-
-@app_views.route('/status', strict_slashes=False)
 def status_all_obj():
 	"""Return status for all obj"""
 	classes = ["Amenity", "City", "Place", "Review", "State", "User"]
